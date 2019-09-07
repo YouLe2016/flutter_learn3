47b7e92b145054926b30e952f49ef6612800efbe
江心才子 <270628297@qq.com>
2019-09-07 14:32:42 +0800

    标题栏和标签栏的小练习

---
+++ lib/constant.dart
@@ -0,0 +1,2 @@
+const imgHeader =
+    "https://i1.hdslb.com/bfs/face/ddf3814320fd7889cd4622f642b33e581801c824.jpg";
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:TestDemo/constant.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,15 +7,82 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+//      debugShowCheckedModeBanner: false,
       title: 'Test Demo',
+      theme: ThemeData(
+//        primaryColor: Colors.pinkAccent,
+          ),
       home: Scaffold(
         appBar: AppBar(
-          title: Row(
-            children: <Widget>[
-              Icon(Icons.arrow_back),
-              Icon(Icons.arrow_back),
-            ],
-          ),
+          elevation: 0,
+          backgroundColor: Colors.pinkAccent,
+//          leading: Icon(Icons.arrow_back),
+          title: Row(children: <Widget>[
+            ClipOval(child: Image.network(imgHeader, width: 32, height: 32)),
+            Flexible(
+              fit: FlexFit.loose,
+              child: Container(
+                margin: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                decoration: BoxDecoration(
+                  shape: BoxShape.rectangle,
+                  color: Colors.black26,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                alignment: Alignment.centerLeft,
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Icon(Icons.search),
+              ),
+            ),
+            Icon(Icons.videogame_asset),
+            SizedBox(width: 20),
+            Icon(Icons.cloud_download),
+            SizedBox(width: 20),
+            Icon(Icons.message),
+          ]),
+        ),
+        body: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                  child: Text("直播"),
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
+                ),
+                Container(
+                  child: Text("推荐"),
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
+                ),
+                Container(
+                  child: Text("追番"),
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
+                ),
+                Container(
+                  child: Text("国家宝藏"),
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
+                ),
+                Container(
+                  child: Text("故事王"),
+                  padding: EdgeInsets.symmetric(vertical: 14),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
+                ),
+              ],
+            )
+          ],
         ),
       ),
     );
