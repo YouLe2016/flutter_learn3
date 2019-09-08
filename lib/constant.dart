afbb94d8445c681b029776a9a8800e6cf908c650
江心才子 <270628297@qq.com>
2019-09-08 17:49:19 +0800

    轮播图图片 分类 关注, 直播分类头

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,2 +1,7 @@
 const imgHeader =
     "https://i1.hdslb.com/bfs/face/ddf3814320fd7889cd4622f642b33e581801c824.jpg";
+const imgBanner =
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567848590259&di=9c56e22268f3df64b866b8a4fbfcdc33&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fblog%2F201405%2F18%2F20140518101548_vNxKG.jpeg";
+const imgBanner_local = 'images/banner.jpg';
+const imgBanner2_gif =
+    "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1567848342863&di=b4424506dab90e428e1e47ab7c1a8ff9&imgtype=0&src=http%3A%2F%2Fpic4.zhimg.com%2Fv2-a55b81d542d328462e34654d600376f3_b.gif";
--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,6 @@ class MyApp extends StatelessWidget {
           title: Row(children: <Widget>[
             ClipOval(child: Image.network(imgHeader, width: 32, height: 32)),
             Flexible(
-              fit: FlexFit.loose,
               child: Container(
                 margin: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
                 decoration: BoxDecoration(
@@ -40,7 +39,7 @@ class MyApp extends StatelessWidget {
             Icon(Icons.message),
           ]),
         ),
-        body: Column(
+        body: ListView(
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -49,38 +48,155 @@ class MyApp extends StatelessWidget {
                   child: Text("直播"),
                   padding: EdgeInsets.symmetric(vertical: 14),
                   decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
+                    border: Border(
+                        bottom: BorderSide(width: 1.0, color: Colors.pink)),
+                  ),
                 ),
                 Container(
                   child: Text("推荐"),
                   padding: EdgeInsets.symmetric(vertical: 14),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
                 ),
                 Container(
                   child: Text("追番"),
                   padding: EdgeInsets.symmetric(vertical: 14),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
                 ),
                 Container(
                   child: Text("国家宝藏"),
                   padding: EdgeInsets.symmetric(vertical: 14),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
                 ),
                 Container(
                   child: Text("故事王"),
                   padding: EdgeInsets.symmetric(vertical: 14),
-                  decoration: BoxDecoration(
-                      border: Border(
-                          bottom: BorderSide(width: 1.0, color: Colors.pink))),
                 ),
               ],
+            ),
+            Padding(
+                padding: EdgeInsets.all(16),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(imgBanner_local),
+                )),
+            GridView.count(
+              //水平子Widget之间间距
+              crossAxisSpacing: 0,
+              //垂直子Widget之间间距
+              mainAxisSpacing: 10.0,
+              //GridView内边距
+              padding: EdgeInsets.all(16.0),
+              //一行的Widget数量
+              crossAxisCount: 5,
+              //子Widget宽高比例
+              childAspectRatio: 1.0,
+              shrinkWrap: true,
+              //子Widget列表
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment),
+                    Text("英雄联盟"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_ind),
+                    Text("绝地求生"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_late),
+                    Text("视频聊天"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_return),
+                    Text("第五人格"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_return),
+                    Text("全部标签"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment),
+                    Text("英雄联盟"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_ind),
+                    Text("绝地求生"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_late),
+                    Text("视频聊天"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_return),
+                    Text("第五人格"),
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Icon(Icons.assignment_return),
+                    Text("全部标签"),
+                  ],
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.black12,
+              height: 1,
+              margin: EdgeInsets.symmetric(horizontal: 16),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: Row(
+                children: <Widget>[
+                  Text("我的关注"),
+                  SizedBox(width: 8),
+                  Text("16小时前", style: TextStyle(color: Colors.black26)),
+                  SizedBox(width: 8),
+                  Text("江心才子"),
+                  Text("直播了唱见台", style: TextStyle(color: Colors.black26)),
+                  Flexible(child: Container(), flex: 1),
+                  Icon(Icons.keyboard_arrow_right, color: Colors.black26)
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.black12,
+              height: 1,
+              margin: EdgeInsets.symmetric(horizontal: 16),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: Row(
+                children: <Widget>[
+                  Text("推荐直播"),
+                  Flexible(child: Container(), flex: 1),
+                  Text("换一换", style: TextStyle(color: Colors.black26)),
+                  Icon(Icons.refresh, size: 16, color: Colors.black26)
+                ],
+              ),
             )
           ],
         ),
