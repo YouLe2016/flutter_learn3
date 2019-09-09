d290d4af179e66d17a1325113ff79712b4ec70c7
江心才子 <270628297@qq.com>
2019-09-09 15:04:40 +0800

    输入框的练习

---
+++ lib/widget/edit_widget.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(EditApp());
+
+class EditApp extends StatefulWidget {
+  @override
+  _EditAppState createState() => _EditAppState();
+}
+
+class _EditAppState extends State<EditApp> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EditWidget',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('EditWidget'),
+        ),
+        body: Padding(
+            padding: EdgeInsets.all(14),
+            child: ListView(
+              children: <Widget>[
+                Container(
+                  padding: EdgeInsets.all(8),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.blue),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: EditableText(
+                    controller: _textEditingController,
+                    focusNode: FocusNode(debugLabel: "Look"),
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.red,
+                    ),
+                    cursorColor: Colors.blue,
+                    backgroundCursorColor: Colors.green,
+                    onChanged: (text) {
+                      debugPrint(text);
+//                  debugPrint(_controller.text);
+                    },
+                  ),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    icon: Icon(Icons.perm_identity),
+                    counterText: "counterText",
+                    helperText: "helperText",
+                    errorText: "errorText",
+                  ),
+                ),
+                TextFormField(),
+              ],
+            )),
+      ),
+    );
+  }
+}
