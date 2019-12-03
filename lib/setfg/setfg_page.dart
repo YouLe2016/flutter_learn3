486a36212f52b8885e3faa65c05f5b0182817c96
江心才子 <270628297@qq.com>
2019-12-03 15:18:08 +0800

    舒尔特方格完成

--- lib/setfg/setfg_page.dart
+++ lib/setfg/setfg_page.dart
@@ -15,9 +15,20 @@ class SetfgWidget extends StatelessWidget {
           ChangeNotifierProvider(create: (context) => SetfgProvider(4))
         ],
         child: Padding(
-          padding: EdgeInsets.all(8),
-          child: GridWidget(),
-        ),
+            padding: EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  flex: 6,
+                  child: GridWidget(),
+                ),
+                Expanded(
+                  child: CountDownWidget(),
+                  flex: 4,
+                ),
+              ],
+            )),
       ),
     );
   }
@@ -36,11 +47,12 @@ class _GridWidgetState extends State<GridWidget>
   Animation<Color> _animation2;
 
   Animation<Color> _currentAnimation;
-  int curNum = -1;
+  int _curIndex = -1;
 
   @override
   void initState() {
     super.initState();
+
     _controller =
         AnimationController(duration: Duration(milliseconds: 250), vsync: this);
     _animation = ColorTween(begin: Colors.white, end: Colors.purpleAccent)
@@ -64,10 +76,19 @@ class _GridWidgetState extends State<GridWidget>
       children: List.generate(provider.data.length, (index) {
         return InkWell(
           onTap: () {
-            curNum = provider.data[index];
-            if (curNum == provider.curNum) {
+            _curIndex = index;
+            provider.startTimer();
+            if (provider.data[_curIndex] == provider.curNum) {
               _currentAnimation = _animation;
-              provider.curNum++;
+              if (provider.curNum++ == 16) {
+                provider.stopTimer();
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text("恭喜过关"),
+                    action: SnackBarAction(label: '知道啦', onPressed: () {}),
+                  ),
+                );
+              }
             } else {
               _currentAnimation = _animation2;
             }
@@ -81,14 +102,36 @@ class _GridWidgetState extends State<GridWidget>
             ),
             decoration: BoxDecoration(
               border: Border.all(color: Colors.black),
-//              color: clickColor(index),
-              color: curNum == provider.data[index]
-                  ? _currentAnimation.value
-                  : Colors.white,
+              color:
+                  _curIndex == index ? _currentAnimation.value : Colors.white,
             ),
           ),
         );
       }),
+    );
+  }
+}
+
+class CountDownWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var provider = Provider.of<SetfgProvider>(context);
+    return Column(
+      children: <Widget>[
+        Text(provider.totalTime.toStringAsFixed(1)),
+        RaisedButton(
+          onPressed: () => provider.restart(),
+          child: Text('重置'),
+        ),
+        RaisedButton(
+          onPressed: () => provider.start(4),
+          child: Text('16格子'),
+        ),
+        RaisedButton(
+          onPressed: () => provider.start(5),
+          child: Text('25格子'),
+        ),
+      ],
     );
   }
 }
--- lib/setfg/setfg_provider.dart
+++ lib/setfg/setfg_provider.dart
@@ -1,17 +1,48 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class SetfgProvider extends ChangeNotifier {
-  var size = 4;
+  int size;
   List<int> data;
-  var curNum = 1;
+  int curNum;
+  double totalTime;
+
+  Timer _timer;
 
   SetfgProvider(this.size) {
-    data = List.generate(size * size, (index) => index + 1);
-    data.shuffle();
+    _init();
   }
 
-  nextNumber() {
-    curNum++;
+  _init() {
+    data = List.generate(size * size, (index) => index + 1)..shuffle();
+    totalTime = 0.0;
+    curNum = 1;
     notifyListeners();
+  }
+
+  startTimer() {
+    if (_timer != null) return;
+    _timer = Timer.periodic(Duration(milliseconds: 100), (_) {
+      totalTime += 0.1;
+      notifyListeners();
+    });
+  }
+
+  stopTimer() {
+    _timer?.cancel();
+  }
+
+  restart() {
+    _init();
+    stopTimer();
+    _timer = null;
+  }
+
+  start(int size) {
+    this.size = size;
+    _init();
+    stopTimer();
+    _timer = null;
   }
 }
