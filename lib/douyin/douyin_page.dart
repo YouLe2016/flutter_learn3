54dc25e49d53517cf9bc07eeadc2e9f8c3396574
江心才子 <270628297@qq.com>
2019-12-09 18:05:55 +0800

    仿抖音首页

--- lib/douyin/douyin_page.dart
+++ lib/douyin/douyin_page.dart
@@ -1,3 +1,4 @@
+import 'package:FlutterDemo/constant.dart';
 import 'package:flutter/material.dart';
 
 class DouYinPage extends StatelessWidget {
@@ -17,17 +18,24 @@ class DouYinPage extends StatelessWidget {
 //            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 //            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text('同城', style: TextStyle(color: Colors.white)),
-              Text('首页', style: TextStyle(color: Colors.white)),
+              getBottomTextWidget('同城', true),
+              getBottomTextWidget('首页', false),
               AddIcon(),
-              Text('消息', style: TextStyle(color: Colors.white)),
-              Text('我', style: TextStyle(color: Colors.white)),
+              getBottomTextWidget('消息', false),
+              getBottomTextWidget('我', false),
             ],
           ),
         ),
       ),
     );
   }
+}
+
+getBottomTextWidget(String content, bool isSelected) {
+  return Text('$content',
+      style: isSelected
+          ? TextStyle(fontSize: 18, color: Colors.white)
+          : TextStyle(fontSize: 14, color: Colors.grey[400]));
 }
 
 class AddIcon extends StatelessWidget {
@@ -97,21 +105,123 @@ class Home extends StatelessWidget {
           width: screenWidth * 0.7,
           height: 150,
           child: Container(
+            child: getBottomContent(),
             decoration: BoxDecoration(color: Colors.redAccent),
+          ),
+        ),
+        Positioned(
+          right: 0,
+          top: screenHeight * 0.3,
+          width: screenWidth * 0.25,
+          height: screenHeight * 0.4,
+          child: Container(
+            child: getButtonList(),
+            decoration: BoxDecoration(color: Colors.orange),
           ),
         ),
         Positioned(
           right: 0,
           bottom: 0,
           width: screenWidth * 0.25,
-          height: screenHeight * 0.5,
+          height: screenWidth * 0.25,
           child: Container(
+            child: RotateAlbum(),
             decoration: BoxDecoration(color: Colors.redAccent),
           ),
         ),
       ],
     );
   }
+}
+
+getButtonList() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.spaceAround,
+    children: <Widget>[
+      Container(
+        height: 60,
+        child: Stack(
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: NetworkImage(imgHeader),
+              radius: 24,
+            ),
+            Positioned(
+              bottom: 0,
+              left: 14,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.red, borderRadius: BorderRadius.circular(10)),
+                padding: EdgeInsets.all(2),
+                child: Icon(Icons.add, size: 16, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+      IconText(icon: Icons.favorite,text: "999k" ),
+      IconText(icon: Icons.feedback,text: "999k" ),
+      IconText(icon: Icons.reply,text: "999k" ),
+    ],
+  );
+}
+
+class IconText extends StatelessWidget {
+  final IconData icon;
+  final String text;
+
+  const IconText({Key key, this.icon, this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Icon(icon, size: 36, color: Colors.white),
+        Text(text,style: TextStyle(color: Colors.white),),
+      ],
+    );
+  }
+}
+
+class RotateAlbum extends StatefulWidget {
+  @override
+  _RotateAlbumState createState() => _RotateAlbumState();
+}
+
+class _RotateAlbumState extends State<RotateAlbum>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  RotationTransition _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    _animation = RotationTransition(
+      turns: Tween(begin: 0.0, end: 1.0).animate(_controller),
+      child: Icon(Icons.music_note),
+    );
+    _controller.repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _animation;
+  }
+}
+
+getBottomContent() {
+  return Column(
+    children: <Widget>[
+      ListTile(
+        title: Text("@人民日报"),
+        subtitle: Text('放大森但撒狂风大放多法减肥塞阀发的拆哦大浮动啊放大森懂啊房东爱抚懂啊'),
+      ),
+//      Marquee(text: "大反派积分卡三翻叮安排三房开怕三发碟哦撒就分开拍撒娇佛牌撒进房戳牌萨基佛牌囧啊"),
+    ],
+  );
 }
 
 class TopBar extends StatefulWidget {
